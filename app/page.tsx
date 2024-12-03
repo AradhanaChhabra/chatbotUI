3116483a6475972a196498c2bad20740f8a1fef4
AradhanaChhabra <aradhanachhabra03@gmail.com>
2024-12-03 19:07:32 +0530

    fix: name

--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { ChatInterface } from "@/components/ui/chatInterface";
 export default function Home() {
 	return (
 		<main className="flex flex-col items-center justify-center bg-[#2B2A27] w-screen h-screen pt-[18px]">
-			<h1 className="text-2xl font-bold mb-4 text-white">Claude AI</h1>
+			<h1 className="text-2xl font-bold mb-4 text-white">AI Chatbot UI</h1>
 			<ChatInterface />
 		</main>
 	);
